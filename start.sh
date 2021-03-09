fa2e339b305a9b79fbce60056d9b944ff0631ed6
ngenohzephenia <ngenohzef@gmail.com>
2021-03-09 18:02:00 +0300

    update my README.md

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 export MAIL_USERNAME='zefmesssi77@gmail.com'
 export MAIL_PASSWORD='51201003@77'
 export SECRET_KEY='messi'
-export DATABASE_URL='postgresql+psycopg2://moringa:Access@localhost/blog'
+export DATABASE_URL='postgresql+psycopg2://moringaaccess:Access@localhost/blog'
 
 python3.6 manage.py server
